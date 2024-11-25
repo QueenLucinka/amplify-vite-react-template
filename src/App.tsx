d5aa313882973e03b7122f49313c30f463f944cd
QueenLucinka <lucia.hedy@gmail.com>
2024-11-25 18:27:34 +0100

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,65 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    // Subscribe to data updates
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+
+    // Cleanup subscription on component unmount
+    return () => subscription.unsubscribe();
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  // Delete function
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
   }
 
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <button onClick={() => client.models.Todo.create({ content: window.prompt("Enter a new todo:") })}>
+        + New Todo
+      </button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+  {todos.map((todo) => (
+    <li
+      key={todo.id}
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "8px",
+        borderBottom: "1px solid #ddd",
+      }}
+    >
+      <span style={{ marginRight: "16px" }}>{todo.content}</span>
+      <button
+        onClick={() => deleteTodo(todo.id)}
+        style={{
+          padding: "4px 8px",
+          backgroundColor: "#f44336",
+          color: "#fff",
+          border: "none",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }}
+      >
+        Delete
+      </button>
+    </li>
+  ))}
+</ul>
+
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
@@ -33,6 +67,7 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
